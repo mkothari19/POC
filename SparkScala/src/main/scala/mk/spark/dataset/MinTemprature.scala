c479ed86bf816b49dfe148ca6086677f4b1ba448
Maneesh Kothari <manishkothari1982@gmail.com>
2020-10-16 12:13:07 +0530

    Min Temprature Job

--- SparkScala/src/main/scala/mk/spark/dataset/MinTemprature.scala
+++ SparkScala/src/main/scala/mk/spark/dataset/MinTemprature.scala
@@ -3,7 +3,7 @@ package mk.spark.dataset
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.types.{StringType,IntegerType,FloatType}
+import org.apache.spark.sql.types.{StringType, IntegerType, FloatType}
 import org.apache.spark.sql.functions.{round}
 
 object MinTemprature {
