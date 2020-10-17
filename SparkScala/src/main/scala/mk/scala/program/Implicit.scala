33b33285690dc167b6b887ca405fedab4aa668a4
Maneesh Kothari <manishkothari1982@gmail.com>
2020-10-17 14:57:38 +0530

    implicit

--- SparkScala/src/main/scala/mk/scala/program/Implicit.scala
+++ SparkScala/src/main/scala/mk/scala/program/Implicit.scala
@@ -4,12 +4,22 @@ object Implicit {
   def main(args: Array[String]): Unit = {
     val value=10
     implicit val multipe=2
-    
     def multiply(implicit by:Int)=value*by
-    
     val result=multiply
-    
     println(result)
+  
+    val str1="Hello"
+    implicit val str2="World!!"
+    
+    def printHello(implicit str3:String)=str1+" "+str3
+    println("Call printHello with no parameter")
+    val result1=printHello
+    println(result1)
+    
+    println("Call printHello with  parameter")
+      val result2=printHello("GUYS")
+    println(result2)
+    
     
   }
 }
