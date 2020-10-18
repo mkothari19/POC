c565f7842261e4a7e6a2a5eae8fd2e683fb954ab
Maneesh Kothari <manishkothari1982@gmail.com>
2020-10-18 11:45:14 +0530

    Overidding

---
+++ SparkScala/src/main/scala/mk/scala/program/opps/Overidding.scala
@@ -0,0 +1,36 @@
+package mk.scala.program.opps
+
+class Bank {
+  
+  def intresRate():Double={
+    0
+  }
+}
+
+class Axis extends Bank{
+  
+   def intresRate():Double={
+    7.2
+  }
+   }
+
+ class ICICI extends Bank{
+    def intresRate():Double={
+    7.5
+  }
+ }
+ 
+ class Yes extends Bank{
+    def intresRate():Double={
+    7.5
+  }
+ }
+ 
+ object BankDriver{
+   def main(args: Array[String]): Unit = {
+     println("AXIS= "+ new Axis().intresRate())
+     println("ICICI= "+ new ICICI().intresRate())
+     println("Yes= "+ new Yes().intresRate())
+     
+   }
+ }
