321dc0ac4efc5c9c2e82ae13c82811826206cce7
Maneesh Kothari <manishkothari1982@gmail.com>
2020-10-20 21:50:22 +0530

    companion

--- SparkScala/src/main/scala/mk/scala/companion/Student.scala
+++ SparkScala/src/main/scala/mk/scala/companion/Student.scala
@@ -1,13 +1,16 @@
 package mk.scala.companion
-
+/*1.A companion object is an object that’s declared in the same file as a class, and has the same name as the class
+2. A companion object and its class can access each other’s private members
+3. A companion object’s apply method lets you create new instances of a class without using the new keyword
+4. A companion object’s unapply method lets you de-construct an instance of a class into its individual components
+ * 
+ */
 class Student(var name:String,var age:Int) {
-  
-  override def toString=s"$name,$age"
   
 }
 
 object Student{
-  
+ // De-construct object instance 
   def unapply(s:Student):String=s"${s.name},${s.age}"
   
 }
