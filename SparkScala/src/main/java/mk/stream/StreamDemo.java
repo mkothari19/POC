16bbe543e17239f63baef666bd987ac9993829e9
Maneesh Kothari <manishkothari1982@gmail.com>
2020-10-20 16:00:16 +0530

    java8

--- SparkScala/src/main/java/mk/stream/StreamDemo.java
+++ SparkScala/src/main/java/mk/stream/StreamDemo.java
@@ -2,6 +2,8 @@ package mk.stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class StreamDemo {
@@ -50,5 +52,35 @@ System.out.println("MAX: "+productA.getPrice());
  */
 Product productB=productList.stream().max((p1,p2)->p1.getPrice() < p2.getPrice()?1:1).get();
 System.out.println("MIN :"+productB.getPrice());
+
+/*
+ * Count
+ * 
+ */
+long count=productList.stream().filter(p->p.getPrice()>30000).count();
+
+System.out.println(count);
+/*
+ * Covert list to set
+ */
+
+Set<Float> set= productList.stream().filter(p->p.getPrice()>30000).map(p->p.getPrice()).collect(Collectors.toSet());
+
+System.out.println(set);
+
+Map<String,Float> map= productList.stream().filter(p->p.getPrice()>30000).collect(Collectors.toMap(p->p.getProductName(),p->p.getPrice()));
+
+System.out.println(map);
+
+/*
+ * Method reference in Stream
+ */
+
+List<Float> priceList= productList.stream().filter(p->p.getPrice()>30000).map(Product::getPrice).collect(Collectors.toList());
+   System.out.println(priceList);
+
 }
+
+
+
 }
