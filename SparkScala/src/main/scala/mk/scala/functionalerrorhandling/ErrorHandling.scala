36c7534b0356b5717e524d252def404a5656ead2
Maneesh Kothari <manishkothari1982@gmail.com>
2020-10-21 10:19:02 +0530

    try,success and failoure

--- SparkScala/src/main/scala/mk/scala/functionalerrorhandling/ErrorHandling.scala
+++ SparkScala/src/main/scala/mk/scala/functionalerrorhandling/ErrorHandling.scala
@@ -1,18 +1,32 @@
 package mk.scala.functionalerrorhandling
 
+import scala.util.{Try,Success,Failure}
+
 object ErrorHandling {
   def main(args: Array[String]): Unit = {
     val result=toInt("1")
      println(result)
+    val sum=add("30","20")
+    println(sum)
     
   }
+  
+  /* Using trios Option.Some and None
+   * 
+   */
   def toInt(n:String):Option[Int]={
       try{
         Some(Integer.parseInt(n.trim()))
-            
       }catch{
         case e: Exception =>None
       }
       
+  }
+  
+      /*
+       * Using trios Try,Success and Failure
+       */
+      def add(a: String,b:String): Try[Int] = Try {
+    Integer.parseInt(a.trim)+ Integer.parseInt(b.trim)
     }
-}
+    }
