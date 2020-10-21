9735089415a6b130cb1d0df18bc47bb4c951492e
Maneesh Kothari <manishkothari1982@gmail.com>
2020-10-21 12:52:33 +0530

    expression

--- SparkScala/src/main/scala/mk/scala/expression/ExpressionMatching.scala
+++ SparkScala/src/main/scala/mk/scala/expression/ExpressionMatching.scala
@@ -14,5 +14,9 @@ object ExpressionMatching {
     case _=>"Your are saying Nothing"  
   }
   
-  
+  def evenOrOdd(num:Int):String = num match {
+    case 1 | 3 | 5 | 7 | 9 =>"odd"
+    case 2 | 4 | 6 | 8 | 10 =>"even"
+    case _ => "some other number"
+}
 }
