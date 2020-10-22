f053fbca72a874b886fd8a41b8343d095168b1b0
Maneesh Kothari <manishkothari1982@gmail.com>
2020-10-22 14:53:34 +0530

    thread practical

---
+++ SparkScala/src/main/java/mk/thread/deadlock/Utils.java
@@ -0,0 +1,15 @@
+package mk.thread.deadlock;
+
+public class Utils {
+	
+	public static void sleep(long mills) {
+		
+	  try {
+		Thread.sleep(mills);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	}
+
+}
