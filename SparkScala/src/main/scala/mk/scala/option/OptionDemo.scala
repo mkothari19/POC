0c9cbcab899ee1693fe8e4ad14f2d116f981457d
Maneesh Kothari <manishkothari1982@gmail.com>
2020-10-29 11:29:30 +0530

    Option

---
+++ SparkScala/src/main/scala/mk/scala/option/OptionDemo.scala
@@ -0,0 +1,19 @@
+package mk.scala.option
+
+import spire.math.Algebraic.Expr
+
+object OptionDemo extends App {
+  
+val capital=Map("Tokyo"->"Japan","France"->"Paris","Delhi"->"India")
+
+println(show(capital get "Delhi"))
+println(show(capital get "XYZ"))
+
+def show(x:Option[String])= x match {
+  case Some(s)=>s
+  case None=>"?"
+    
+ 
+}
+  
+}
